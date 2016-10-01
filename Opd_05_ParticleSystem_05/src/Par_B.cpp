7b3049788847db1370f9d76e54f3cfdb3fc620e2
tuursangers <tuursangers@hotmail.com>
2016-10-01 16:08:28 +0200

    Opd 5 New

---
+++ Opd_05_ParticleSystem_05/src/Par_B.cpp
@@ -0,0 +1,26 @@
+#include "Par_B.h"
+
+Par_B::Par_B(int startX, int startY) : Particle(startX, startY) {
+
+	//centerColor = ofColor(255, 255, 255, 255);
+	//innerColor = ofColor(251,255,67, 8);
+	//outerColor = ofColor(251, 255, 67, 1);
+	curve = ofVec2f(ofRandom(-5, 5), ofRandom(-5, 5));
+}
+
+void Par_B::move() {
+	
+	curve.rotate(2);
+	position += curve;
+
+}
+
+void Par_B::positionColor() {
+
+	centerColor = ofColor(255, 255, 255, 255);
+	innerColor = ofColor(255, 255, 255, 255);
+	outerColor = ofColor(255, 255, 255, 255);
+
+}
+
+
